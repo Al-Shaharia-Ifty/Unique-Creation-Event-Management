21ab17fdb245c55ddb277dd48b89c4f00342c7a4
Al Shaharia <ifty682002@gmail.com>
2022-09-01 19:25:15 +0600

    navbar almost done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
-import Navbar2 from "./Components/Navbar2";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Navbar2 />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
